f63c0253ea0765704e761814427b4468391979a0
Liz Kinker <Liz.Kinker@gmail.com>
2023-07-28 08:32:49 -0400

    No changes

--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,15 +1,22 @@
 package rocks.zipcode.io;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 /**
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+    Item[] items;
     public ItemSorter(Item[] items) {
+        this.items = items;
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+        List<Item> list = Arrays.asList(items);
+        list.sort(comparator);
+        return list.toArray(new Item[items.length]);
     }
 }
